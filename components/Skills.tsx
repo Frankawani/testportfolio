ef2608c01387e8a23003f44f8cc2e9e937da232b
Frank Awani <awanifrank@gmail.com>
2023-05-17 13:01:24 +0100

    all done

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -12,16 +12,16 @@ function Skills({ skills }: Props) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className=" relative min-h-screen flex flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:p-10 justify-center xl:space-y-0 mx-auto items-center"
+      className=" relative min-h-screen flex flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:p-10 justify-center xl:space-y-32 mx-auto items-center"
     >
       {" "}
       <h3 className=" absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         skills
       </h3>
-      <h4 className=" absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm hidden md:inline-flex">
+      <h4 className=" absolute top-5 uppercase tracking-[3px] text-gray-500 text-sm hidden md:inline-flex">
         Hover over a skill to see proficiency
       </h4>
-      <div className=" grid grid-cols-4 gap-4">
+      <div className=" grid grid-cols-4 gap-4 ">
         {skills?.slice(0, skills.length / 2).map((skill) => (
           <Skill key={skill._id} skill={skill} />
         ))}
